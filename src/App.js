1612eba9cb40e11badd49bd578f4bb1e5cce754e
dev-from-cbd <veryfatwombat@gmail.com>
2023-08-31 06:31:09 +0930

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the logo from the 'logo.svg' file
+import logo from "./logo.svg";
 
+// Import styles from the 'App.css' file
+import "./App.css";
+
+// Define the App function component
 function App() {
+  // Return a JSX element representing the component
   return (
+    // Outer container with the class "App"
     <div className="App">
+      {/* Header section with the class "App-header" */}
       <header className="App-header">
+        {/* Image element with the 'logo' as the source, class "App-logo", and alternative text "logo" */}
         <img src={logo} className="App-logo" alt="logo" />
+        {/* Paragraph with text and a code element */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Link with class "App-link", pointing to 'reactjs.org', set to open in a new tab, and having alternative text */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +32,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export
 export default App;
