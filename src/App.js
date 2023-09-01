ee948330da53c7b9de90a5b1bd75a4f0b5c62c0d
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-02 07:32:48 +0930

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// Import the logo from the 'logo.svg' file
-import logo from "./logo.svg";
-
-// Import styles from the 'App.css' file
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 // Define the App function component
 function App() {
@@ -10,24 +7,7 @@ function App() {
   return (
     // Outer container with the class "App"
     <div className="App">
-      {/* Header section with the class "App-header" */}
-      <header className="App-header">
-        {/* Image element with the 'logo' as the source, class "App-logo", and alternative text "logo" */}
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* Paragraph with text and a code element */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* Link with class "App-link", pointing to 'reactjs.org', set to open in a new tab, and having alternative text */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Home Page</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Import necessary functions from testing libraries
+import { render, screen } from "@testing-library/react";
+// Import the App component to be tested
+import App from "./App";
 
-test('renders learn react link', () => {
+// Test case: renders the 'learn react' link in the App component
+test("renders learn react link", () => {
+  // Render the App component
   render(<App />);
+  // Search for an element containing the text 'learn react' case-insensitively
   const linkElement = screen.getByText(/learn react/i);
+  // Expect the 'learn react' link element to be in the document
   expect(linkElement).toBeInTheDocument();
 });
