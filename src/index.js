8fbfbd7495e216e28496a8c58a9015a20eca3222
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-03 08:42:51 +0930

    Comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
+// Import React library for creating and managing components.
 import React from "react";
+
+// Import ReactDOM for rendering React components to the DOM.
 import ReactDOM from "react-dom/client";
+
+// Import the CSS file for styling the application.
 import "./index.css";
+
+// Import the main application component.
 import App from "./App";
+
+// Import a function for reporting web vitals.
 import reportWebVitals from "./reportWebVitals";
 
+// Import Bootstrap CSS for styling.
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Create a React root for rendering the application.
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the main App component within a React Strict Mode.
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Use reportWebVitals to measure and log performance metrics.
 reportWebVitals();
