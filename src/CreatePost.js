fdcbd9cf468c413a903bf2af5ae9131d105f799e
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-10 07:35:38 +0930

    Update

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,15 +1,35 @@
 import "./App.css";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { handle } from "express/lib/application";
 
 function CreatePost() {
   const navigate = useNavigate();
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+  });
 
   return (
     <div className="div_createPost">
       <h1>Create a post</h1>
       <Form>
-        <Form.Group>Form</Form.Group>
+        <Form.Group>
+          <Form.Control
+            name="title"
+            value={title}
+            placeholder="Title"
+            className="form_Control_title"
+            onChange={handleChange}
+          />
+          <Form.Control
+            name="description"
+            placeholder="Description"
+            className="form_Control_description"
+            onChange={handleChange}
+          />
+        </Form.Group>
       </Form>
       <Button className="button_createPost" onClick={() => navigate(-1)}>
         BACK
