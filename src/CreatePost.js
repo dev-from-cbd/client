4e647ab14f1cb79b8980e9c29e69f8ff7ce4fb58
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-16 07:00:05 +0930

    Update

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,40 +1,65 @@
 import "./App.css";
+// Importing a CSS file named "App.css" for styling.
+
 import { Button, Form } from "react-bootstrap";
+// Importing Button and Form components from the "react-bootstrap" library.
+
 import { useNavigate } from "react-router-dom";
+// Importing the useNavigate function from the "react-router-dom" library.
+
 import { useState } from "react";
+// Importing the useState hook from the "react" library.
+
 import axios from "axios";
+// Importing the axios library for making HTTP requests.
 
 function CreatePost() {
+  // Declaring a functional component named "CreatePost."
+
   const navigate = useNavigate();
+  // Initializing the "navigate" function using the useNavigate hook.
+
   const [post, setPost] = useState({
     title: "",
     description: "",
   });
+  // Initializing a state variable "post" using the useState hook with initial values for "title" and "description."
 
   const handleChange = (event) => {
+    // Defining a function "handleChange" that takes an event as an argument.
     const { name, value } = event.target;
+    // Destructuring "name" and "value" from the event target.
+
     setPost((prev) => {
       return {
         ...prev,
         [name]: value,
       };
     });
+    // Updating the "post" state by merging the previous state and changing the specified field.
   };
 
   const handleClick = (event) => {
+    // Defining a function "handleClick" that takes an event as an argument.
     event.preventDefault();
+    // Preventing the default form submission behavior.
 
     axios
       .post("/create", post)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
+    // Making a POST request to "/create" with the "post" data, and logging the response or error.
   };
 
   return (
+    // Returning JSX for rendering the component.
     <div className="div_createPost">
       <h1>Create a post</h1>
+      {/* Displaying a heading "Create a post." */}
       <Form>
+        {/* Starting a form using the Form component from "react-bootstrap." */}
         <Form.Group>
+          {/* Creating a form group. */}
           <Form.Control
             name="title"
             value={post.title}
@@ -42,6 +67,7 @@ function CreatePost() {
             className="form_Control_title"
             onChange={handleChange}
           />
+          {/* Input field for "title" with value, placeholder, class, and onChange event. */}
           <Form.Control
             name="description"
             value={post.description}
@@ -49,6 +75,7 @@ function CreatePost() {
             className="form_Control_description"
             onChange={handleChange}
           />
+          {/* Input field for "description" with value, placeholder, class, and onChange event. */}
         </Form.Group>
         <Button
           className="button_createPost"
@@ -57,12 +84,15 @@ function CreatePost() {
         >
           Create post
         </Button>
+        {/* Button for creating a post with class and onClick event. */}
       </Form>
       <Button className="button_back" onClick={() => navigate(-1)}>
         BACK
       </Button>
+      {/* Button for going back with class and onClick event. */}
     </div>
   );
 }
 
 export default CreatePost;
+// Exporting the CreatePost component as the default export.
