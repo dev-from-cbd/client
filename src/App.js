8dfafa26b7108da1d34cba976c4d9aab5d91efb6
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-17 04:27:22 +0930

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+// Import the CSS file associated with the App component.
 import "./App.css";
+// Import the Button component from the React Bootstrap library.
 import { Button } from "react-bootstrap";
+// Import the useNavigate hook from the react-router-dom library.
 import { useNavigate } from "react-router-dom";
 
+// Define the App component.
 function App() {
+  // Initialize the navigate function using the useNavigate hook.
   const navigate = useNavigate();
 
   return (
+    // Render the App component.
     //<div className="App">
     <div className="className_App">
       <h1>Home Page</h1>
+      {/* Render a Button component with specific props. */}
       <Button
         variant="outline-dark"
         className="button_App"
+        // Set an onClick event to navigate to the "create" route.
         onClick={() => navigate("create")}
       >
         NEXT
@@ -20,4 +28,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export of this module.
 export default App;
