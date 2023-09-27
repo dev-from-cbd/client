29e42ac6bdb196e4e23f189a99ea2282b0a84887
dev-from-cbd <veryfatwombat@gmail.com>
2023-09-28 06:39:06 +0930

    Update

--- src/Posts.js
+++ src/Posts.js
@@ -16,6 +16,13 @@ function Posts() {
       .catch((err) => err);
   }, []);
 
+  const deletePost = (id) => {
+    axios
+      .delete(`/delete/${id}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="div_Posts">
       <h1>H1</h1>
@@ -23,12 +30,20 @@ function Posts() {
         <>
           {posts.map((post) => {
             return (
-              <div className="div_Post">
+              <div key={post._id} className="div_Post">
                 <h4>{post.title}</h4>
                 <p>{post.description}</p>
                 <div className="div_Post_Button">
-                  <Button className="post_Button">Update</Button>
-                  <Button>Delete</Button>
+                  <Button variant="outline-info" className="post_Button_Update">
+                    Update
+                  </Button>
+                  <Button
+                    onClick={() => deletePost(post._id)}
+                    variant="outline-danger"
+                    className="post_Button_Delete"
+                  >
+                    Delete
+                  </Button>
                 </div>
               </div>
             );
